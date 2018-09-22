d1bc3a0fe4d8e920ff5097eeb3437cd709ddb968
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-23 00:43:55 +0200

    Added useful content to ListPuhElementView.

--- frontend/src/use-cases/list-puh/screens/list-puh-screen/views/list-puh-element-view/ListPuhElementView.jsx
+++ frontend/src/use-cases/list-puh/screens/list-puh-screen/views/list-puh-element-view/ListPuhElementView.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { DigitDesign, DigitText } from "@cthit/react-digit-components";
-import { ListPuhElementViewSpacing } from "./ListPuhElementView.styles";
+import { DigitDesign, DigitText, DigitButton } from "@cthit/react-digit-components";
+import { ListPuhElementViewSpacing, ListPuhElementViewMargin } from "./ListPuhElementView.styles";
+import Space from "../../../../../../common-ui/views/space"
 
 export const ListPuhElementView = () => (
   <div>
-    <DigitDesign.Card absHeight="200px">
-      <DigitText.Text text="This is a PUH for the selected committe, someNick, someCid, somePersonalSequirityNumber, someAddress, somePhoneNumber, I HOPE NOBODY SEES THIS!" />
+    <DigitDesign.Card absHeight="100px">
+      <ListPuhElementViewMargin>
+        <Space>
+          <DigitText.Heading text="Some list pdp"/>
+          <DigitText.Heading text="2018-02-05"/>
+        </Space>
+        <Space>
+          <DigitButton raised secondary text="Remove"/>
+          <DigitButton raised primary text="Details"/>
+        </Space>
+      </ListPuhElementViewMargin>
     </DigitDesign.Card>
     <ListPuhElementViewSpacing />
   </div>
