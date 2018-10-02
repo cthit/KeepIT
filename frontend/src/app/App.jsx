2e757f71a793fd8a1065e889560d58e003a7de6d
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-10-02 18:07:43 +0200

    Add routing.

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 import { DigitProviders, DigitHeader } from "@cthit/react-digit-components";
 import ListPuh from "../use-cases/list-puh";
 import DrawerView from "../common/views/drawerView";
+import { Switch, Route } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
       <DigitProviders>
-        <DigitHeader
-          title="KeepIT"
-          renderMain={() => <ListPuh />}
-          renderDrawer={() => <DrawerView />}
-        />
+        <div>
+          <DigitHeader
+            title="KeepIT"
+            renderMain={() => <ListPuh />}
+            renderDrawer={() => <DrawerView />}
+          />
+
+          <Switch>
+            <Route path="/" component={ListPuh}/>
+            <Route path="/add" component={ListPuh}/>
+          </Switch>
+        </div>
       </DigitProviders>
     );
   }
