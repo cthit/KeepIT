da1314cdbb1bf85fbfa054d508f152bd2197ff70
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-02-19 18:41:10 +0100

    Now able to switch between add/list pages.

--- frontend/src/common/views/headerButtonsView/HeaderButtons.view.jsx
+++ frontend/src/common/views/headerButtonsView/HeaderButtons.view.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { DigitButton, DigitDesign } from "@cthit/react-digit-components";
 import { ButtonContainer } from "./HeaderButtons.view.styles.jsx";
+import { Switch, Route } from "react-router-dom";
 
 export const HeaderButtons = props => (
     <div style={{ display: "flex" }}>
         <ButtonContainer>
-            <DigitDesign.Link to={"/add"}>
-                <DigitButton text="Add PDP" raised primary />
-            </DigitDesign.Link>
+            <Switch>
+                <Route
+                    path="/add"
+                    component={() => (
+                        <DigitDesign.Link to={"/"}>
+                            <DigitButton text="List PDP" raised primary />
+                        </DigitDesign.Link>
+                    )}
+                />
+                <Route
+                    path="/"
+                    component={() => (
+                        <DigitDesign.Link to={"/add"}>
+                            <DigitButton text="Add PDP" raised primary />
+                        </DigitDesign.Link>
+                    )}
+                />
+            </Switch>
         </ButtonContainer>
         <ButtonContainer>
             <DigitButton text="Login" raised primary />
